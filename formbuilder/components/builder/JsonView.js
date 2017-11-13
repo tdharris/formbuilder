3de61847b7b25bbd153d6d40e8e407263bc67782
tdharris <tylerdavidharris@gmail.com>
2017-11-12 19:21:39 -0700

    remove editSchema from uiSchema (not used in mozilla-services/react-jsonschema-form)

--- formbuilder/components/builder/JsonView.js
+++ formbuilder/components/builder/JsonView.js
@@ -1,8 +1,11 @@
 import React from "react";
 
+var omitDeep = require('omit-deep');
 
 export default function JSONView(props) {
-  return (
+
+return (
+  <div>
     <div>
       <h3>JSONSchema</h3>
       <div className="form-group">
@@ -14,6 +17,19 @@ export default function JSONView(props) {
           value={JSON.stringify(props.schema, null, 2)}
           onChange={() => {}} />
       </div>
+    </div> 
+    <div>
+      <h3>uiSchema</h3>
+      <div className="form-group">
+        <textarea
+          className="form-control"
+          readOnly=""
+          rows="20"
+          style={{fontFamily: "monospace"}}
+          value={JSON.stringify(omitDeep(props.uiSchema, ['editSchema']), null, 2)}
+          onChange={() => {}} />
+      </div>
     </div>
-  );
+  </div>
+);
 }
